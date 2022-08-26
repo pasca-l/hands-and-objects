0e9e3ed71b2198f63a59159f184a2c5c0807d941
pasca-l <shion.yamadate@keio.jp>
2022-08-26 13:35:15 +0900

    updated system

--- pnr_temporal_localization/models/hand_salience.py
+++ pnr_temporal_localization/models/hand_salience.py
@@ -1,6 +1,7 @@
-from turtle import forward
+import torch
 import torch.nn as nn
 import torch.optim as optim
+import mediapipe as mp
 
 
 class System():
@@ -18,20 +19,57 @@ class System():
 class HandSalience(nn.Module):
     def __init__(self):
         super().__init__()
-        self.hand_detect = HandDetect()
+        self.hand_saliency_map = AddHandMapTransform()
+        self.resnet3d = torch.hub.load('facebookresearch/pytorchvideo',
+                                  'slow_r50', pretrained=True)
 
     def forward(self, x):
-        x = self.hand_detect(x)
+        x = self.hand_saliency_map(x)
+        print(x)
+        # x = self.resnet3d(x)
 
         return x
 
 
-class HandDetect():
+class AddHandMapTransform():
     def __init__(self):
-        pass
+        self.image_mode = True
+        self.hand_num = 2
+        self.detection_conf = 0.7
+
+        mp_hands = mp.solutions.hands
+        self.hands = mp_hands.Hands(
+            static_image_mode=self.image_mode,
+            max_num_hands=self.hand_num,
+            min_detection_confidence=self.detection_conf
+        )
 
     def __call__(self, frames):
-        frame_num = frames.shape[2]
+        images = frames.detach().numpy().transpose(0,2,3,4,1).astype('uint8')
+
+        coords = self.detect_hand_coord(images)
+        self.coords_to_map()
+
+        return coords
+
+    def detect_hand_coord(self, images):
+        coords = []
+        batch_size = images.shape[0]
+        frame_num = images.shape[1]
+        h, w = images.shape[2], images.shape[3]
+
+        for batch in range(batch_size):
+            for i in range(frame_num):
+                results = self.hands.process(images[batch,:,i,:,:])
+
+                if results.multi_hand_landmarks is None:
+                    return coords
+
+                else:
+                    return None
+
+    def coords_to_map(self):
+        pass
 
 
 class IdentityTransform():
