a5fe11924f867b65924e8d716623ba055b0f3e4f
pasca-l <shion.yamadate@keio.jp>
2022-10-05 08:21:57 +0000

    changed to loading frame images

--- pnr_temporal_localization/dataset_module.py
+++ pnr_temporal_localization/dataset_module.py
@@ -1,4 +1,3 @@
-import sys
 import cv2
 import numpy as np
 import torch
@@ -6,31 +5,20 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import pytorch_lightning as pl
 
-sys.path.append("../utils")
-from json_handler import JsonHandler
-from video_extractor import Extractor
-
 
 class PNRTempLocDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, ann_dir, ann_task_name='fho_hands',
-                 batch_size=1):
+    def __init__(self, batch_size, data_dir, json_dict):
         super().__init__()
-        self.ann_task_name = ann_task_name
-        self.path_dict = {
-            "data_dir": data_dir,
-            "train_json": f"{ann_dir}{ann_task_name}_train.json",
-            "val_json": f"{ann_dir}{ann_task_name}_val.json",
-            "test_json": f"{ann_dir}{ann_task_name}_test_unannotated.json"
-        }
+        self.data_dir = data_dir
+        self.json_dict = json_dict
         self.batch_size = batch_size
     
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
-            json_handler = JsonHandler(self.ann_task_name)
             self.train_data =\
-                PNRTempLocDataset('train', json_handler, **self.path_dict)
+                PNRTempLocDataset(self.data_dir, self.json_dict['train'])
             self.val_data =\
-                PNRTempLocDataset('val', json_handler, **self.path_dict)
+                PNRTempLocDataset(self.data_dir, self.json_dict['val'])
 
         if stage == "test" or stage is None:
             self.test_data = None
@@ -71,18 +59,12 @@ class PNRTempLocDataModule(pl.LightningDataModule):
 
 
 class PNRTempLocDataset(Dataset):
-    def __init__(self, phase, json_handler, extraction=False, **path_dict):
-        self.data_dir = path_dict["data_dir"]
-        self.json_file = path_dict[f"{phase}_json"]
-        self.clip_dir = f"{self.data_dir}clips/"
-        # self.action_frame_dir = f"{self.data_dir}action_frames/"
+    def __init__(self, data_dir, flatten_json):
+        self.clip_dir = f"{data_dir}clips/"
+        self.frame_dir = f"{data_dir}frames/"
+        self.flatten_json = flatten_json
 
         self.transform = FrameTransform()
-
-        self.flatten_json = json_handler(self.json_file)
-        # if extraction:
-        #     extractor = Extractor(self.data_dir, self.flatten_json)
-        #     extractor.extract_action_clip_frame()
 
     def __len__(self):
         return len(self.flatten_json)
@@ -90,22 +72,13 @@ class PNRTempLocDataset(Dataset):
     def __getitem__(self, index):
         info = self.flatten_json[index]
 
-        # video_path = f"{self.data_dir}{info['clip_uid']}.mp4"
-        # video = cv2.VideoCapture(video_path)
-        # info["original_fps"] = video.get(cv2.CAP_PROP_FPS)
-
-        frames, labels, fps, frame_nums = self._sample_clip_with_label(info)
+        frames, labels = self._sample_clip_with_label(info)
         frames = torch.as_tensor(frames, dtype=torch.float).permute(3, 0, 1, 2)
         frames = self.transform(frames)
-
-        info["sample_frame_num"] = frame_nums
-        info["effective_fps"] = fps
 
         return frames, labels, info
 
     def _sample_clip_with_label(self, info):
-        # fps = info["original_fps"]
-        fps = 30
         start_frame = info["clip_start_frame"]
         end_frame = info["clip_end_frame"]
         pnr_frame = info["clip_pnr_frame"]
@@ -117,31 +90,22 @@ class PNRTempLocDataset(Dataset):
             self._sample_out_frames(
                 pnr_frame, random_start_frame, random_end_frame, 32)
 
-        # frames = []
-        # for frame_num in sample_frame_num:
-        #     frame_path = f"{self.action_frame_dir}{info['clip_uid']}" +\
-        #                  f"/{frame_num}.png"
-        #     try:
-        #         image = self._load_frame(frame_path)
-        #     except:
-        #         print(f"Image does not exist : {frame_path}")
-        #         return
-        #     frames.append(image)
-
         frames = self._load_frames(sample_frame_num, info)
 
         keyframe_idx = np.argmin(frame_pnr_dist)
         onehot_label = np.zeros(len(sample_frame_num))
         onehot_label[keyframe_idx] = 1
 
-        clipped_seconds = (random_end_frame - random_start_frame) / fps
-        effective_fps = len(sample_frame_num) / clipped_seconds
+        # fps = info["original_fps"]
+        # clipped_seconds = (random_end_frame - random_start_frame) / fps
+        # effective_fps = len(sample_frame_num) / clipped_seconds
+
+        # info["sample_frame_num"] = sample_frame_num
+        # info["effective_fps"] = effective_fps
 
         return (
             np.concatenate(frames),
-            np.array(onehot_label, dtype='float32'),
-            effective_fps,
-            sample_frame_num
+            np.array(onehot_label, dtype='float32')
         )
 
     def _random_clipping(self, pnr, start_frame, end_frame, min_ratio):
@@ -183,39 +147,49 @@ class PNRTempLocDataset(Dataset):
         )
 
     def _load_frames(self, frame_nums, info):
-        video_path = f"{self.clip_dir}{info['clip_uid']}.mp4"
-        video = cv2.VideoCapture(video_path)
-        info["original_fps"] = video.get(cv2.CAP_PROP_FPS)
-
-        if not video.isOpened():
-            print(f"Video cannot be opened : {video_path}")
-            return
-
         frames = []
-        counter = 1
 
-        while True:
-            ret, frame = video.read()
-            if ret == False:
-                break
-            if counter in sorted(frame_nums):
+        for num in frame_nums:
+            frame_path = f"{self.frame_dir}{info['clip_uid']}/{num}.png"
+
+            try:
+                frame = cv2.imread(frame_path)
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 frame = cv2.resize(frame, (224, 224))
                 frame = np.expand_dims(frame, axis=0).astype(np.float32)
-                frames.append(frame)
+            except:
+                print(f"Image does not exist : {frame_path}")
+                return
 
-            counter += 1
+            frames.append(frame)
 
-        video.release()
         return frames
 
-    # def _load_frame(self, frame_path):
-    #     frame = cv2.imread(frame_path)
-    #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    #     frame = cv2.resize(frame, (224, 224))
-    #     frame = np.expand_dims(frame, axis=0).astype(np.float32)
+        # video_path = f"{self.clip_dir}{info['clip_uid']}.mp4"
+        # video = cv2.VideoCapture(video_path)
+        # info["original_fps"] = video.get(cv2.CAP_PROP_FPS)
 
-    #     return frame
+        # if not video.isOpened():
+        #     print(f"Video cannot be opened : {video_path}")
+        #     return
+
+        # frames = []
+        # counter = 1
+
+        # while True:
+        #     ret, frame = video.read()
+        #     if ret == False:
+        #         break
+        #     if counter in sorted(frame_nums):
+        #         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #         frame = cv2.resize(frame, (224, 224))
+        #         frame = np.expand_dims(frame, axis=0).astype(np.float32)
+        #         frames.append(frame)
+
+        #     counter += 1
+
+        # video.release()
+        # return frames
 
 
 class FrameTransform():
--- pnr_temporal_localization/main.py
+++ pnr_temporal_localization/main.py
@@ -1,4 +1,7 @@
+import json
+import sys
 import argparse
+import shutil
 import importlib
 import torch
 import pytorch_lightning as pl
@@ -6,19 +9,26 @@ import pytorch_lightning as pl
 from dataset_module import PNRTempLocDataModule
 from system import PNRLocalizer
 
+sys.path.append("../utils")
+from json_handler import JsonHandler
+from video_extractor import Extractor
+
 
 def option_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--phase', type=str, default='train',
                         choices=['train', 'test'])
+    parser.add_argument('--task', type=str, default="fho_hands",
+                        choices=["fho_hands"])
     parser.add_argument('--log_save_dir', type=str, default='./logs/')
+    parser.add_argument('--delete_log_dir', action='store_true')
     parser.add_argument('--ann_dir', type=str,
                         default='../../../data/ego4d/annotations/')
     parser.add_argument('--data_dir', type=str, 
                         default='../../../data/ego4d/')
     parser.add_argument('--model', type=str, default="cnnlstm",
-        choices=["cnnlstm", "slowfastperceiver", "bmn", "i3d_resnet",
-                 "hand_salience"])
+                        choices=["cnnlstm", "slowfastperceiver", "bmn", 
+                                 "i3d_resnet", "hand_salience"])
 
     return parser.parse_args()
 
@@ -26,11 +36,21 @@ def option_parser():
 def main():
     args = option_parser()
 
+    if args.delete_log_dir:
+        shutil.rmtree(args.log_save_dir)
+
+    json_handler = JsonHandler(args.task)
+    json_partial_name = f"{args.ann_dir}{args.task}"
+    json_dict = {
+        "train": json_handler(f"{json_partial_name}_train.json"),
+        "val": json_handler(f"{json_partial_name}_val.json"),
+        # "test": json_handler(f"{json_partial_name}_test_unannotated.json")
+    }
+
     dataset = PNRTempLocDataModule(
+        batch_size=4,
         data_dir=args.data_dir,
-        ann_dir=args.ann_dir,
-        ann_task_name="fho_hands",
-        batch_size=4
+        json_dict=json_dict
     )
 
     module = importlib.import_module(f'models.{args.model}')
@@ -54,8 +74,6 @@ def main():
         accelerator='auto',
         devices='auto',
         auto_select_gpus=True,
-        # accelerator='gpu',
-        # devices=1,
         max_epochs=10,
         logger=logger,
         callbacks=[checkpoint_callback]
