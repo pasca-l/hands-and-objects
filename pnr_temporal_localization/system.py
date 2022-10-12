856c60920272afd38f75573b50509b0c4e7de3eb
pasca-l <shion.yamadate@keio.jp>
2022-10-12 12:36:09 +0900

    added calculation of accuracy

--- pnr_temporal_localization/system.py
+++ pnr_temporal_localization/system.py
@@ -1,5 +1,5 @@
 import pytorch_lightning as pl
-# import torchmetrics
+import torchmetrics
 
 
 class PNRLocalizer(pl.LightningModule):
@@ -11,7 +11,7 @@ class PNRLocalizer(pl.LightningModule):
         self.label_function = sys.label_transform
 
         # self.train_acc = torchmetrics.Accuracy()
-        # self.val_acc = torchmetrics.Accuracy()
+        self.val_acc = torchmetrics.Accuracy()
 
     def training_step(self, batch, batch_idx):
         frames = batch[0]
@@ -20,6 +20,8 @@ class PNRLocalizer(pl.LightningModule):
         loss = self.loss(logits, label)
 
         self.log("train_loss", loss, on_step=True)
+        # self.train_acc(logits, label)
+        # self.log("train_acc", self.train_acc, on_step=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -29,6 +31,8 @@ class PNRLocalizer(pl.LightningModule):
         loss = self.loss(logits, label)
 
         self.log("val_loss", loss, on_step=False, on_epoch=True)
+        self.val_acc(logits, label)
+        self.log("val_acc", self.val_acc, on_step_=False, on_epoch=True)
 
     def configure_optimizers(self):
         return self.optimizer
