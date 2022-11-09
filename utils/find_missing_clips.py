83df51cd0ff2adfa8e1156a2c80aaf338f4bf4d5
pasca-l <shion.yamadate@keio.jp>
2022-11-09 11:47:42 +0000

    fixed path

--- utils/find_missing_clips.py
+++ utils/find_missing_clips.py
@@ -4,18 +4,17 @@ from json_handler import JsonHandler
 
 
 def main():
-    ann_dir = '~/data/ego4d/annotations/'
-    task_name = 'fho_scod'
+    ann_dir = '/home/ubuntu/data/ego4d/annotations/'
+    task_name = 'fho_hands'
 
     json_handler = JsonHandler(task_name)
     json_partial_name = ann_dir + task_name
     json_dict = {
         "train": json_handler(f"{json_partial_name}_train.json"),
         "val": json_handler(f"{json_partial_name}_val.json"),
-        # "infer": json_handler(f"{json_partial_name}_test_unannotated.json")
     }
 
-    files_path = glob.glob("~/data/ego4d/clips/*")
+    files_path = glob.glob("/home/ubuntu/data/ego4d/clips/*")
     files = [os.path.splitext(os.path.basename(i))[0] for i in files_path]
 
     clips = set()
