f67e267e57ba7962a616e1da28184790600d6d4f
pasca-l <shion.yamadate@keio.jp>
2023-04-05 00:08:56 +0900

    simplified unet system

--- objectness_classification/datasets/transform.py
+++ objectness_classification/datasets/transform.py
@@ -4,22 +4,14 @@ from torchvision import transforms
 
 class ObjnessClsDataPreprocessor:
     def __call__(self, x):
-        transform = self._base_transform()
-        return transform(x)
-
-    def _base_transform(self):
         transform = transforms.Compose([
             transforms.ToTensor()
         ])
+#     transform = transforms.Compose([
+#         transforms.Lambda(
+#             lambda x: torch.as_tensor(x, dtype=torch.float)
+#         ),
+#         transforms.Normalize([0.45], [0.225])
+#     ])
 
-        return transform
-
-    # def _simple_transform(self):
-    #     transform = transforms.Compose([
-    #         transforms.Lambda(
-    #             lambda x: torch.as_tensor(x, dtype=torch.float)
-    #         ),
-    #         transforms.Normalize([0.45], [0.225])
-    #     ])
-
-    #     return transform
+        return transform(x)
--- objectness_classification/models/unet.py
+++ objectness_classification/models/unet.py
@@ -7,7 +7,13 @@ import segmentation_models_pytorch as smp
 
 class System():
     def __init__(self):
-        self.model = Unet()
+        self.model = smp.Unet(
+            encoder_name="resnet101",
+            encoder_weights="imagenet",
+            in_channels=3,
+            classes=2,
+            activation="sigmoid",
+        )
         self.loss = smp.losses.DiceLoss(
             mode='binary'
         )
@@ -15,37 +21,3 @@ class System():
             self.model.parameters(),
             lr=0.001
         )
-
-
-class Unet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.unet = smp.Unet(
-            encoder_name="resnet101",
-            encoder_weights="imagenet",
-            in_channels=3,
-            classes=1
-        )
-
-    def forward(self, x):
-        x = self.unet(x)
-
-        return x
-
-
-# class DiceLoss(nn.Module):
-#     def __init__(self, weight=None, size_average=True):
-#         super().__init__()
-
-#     def forward(self, inputs, targets, smooth=1):
-#         targets = targets[:,1:,:,:]
-#         inputs = nnf.sigmoid(inputs)
-
-#         inputs = torch.flatten(inputs)
-#         targets = torch.flatten(targets)
-
-#         intersection = (inputs * targets).sum()
-#         dice = (2.*intersection + smooth) / \
-#                (inputs.sum() + targets.sum() + smooth)
-
-#         return 1 - dice
