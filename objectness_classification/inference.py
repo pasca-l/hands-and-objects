b3eafba9f85a526d046d72ce7335bc5326c7628f
pasca-l <shion.yamadate@keio.jp>
2023-04-17 20:39:25 +0900

    updated inference code

--- objectness_classification/inference.py
+++ objectness_classification/inference.py
@@ -1,25 +1,25 @@
+import os
 import sys
 import argparse
-import shutil
 import importlib
+import git
 import pytorch_lightning as pl
 
-from dataset_module import ObjnessClsDataModule
+git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
+git_root = git_repo.git.rev_parse("--show-toplevel")
+sys.path.append(f"{git_root}/objectness_classification/datasets")
+from datamodule import ObjnessClsDataModule
 from system import ObjnessClassifier
-
-sys.path.append("../utils")
-from json_handler import JsonHandler
+from seed import set_seed
 
 
 def option_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--task', type=str, default="fho_scod",
-                        choices=["fho_scod"])
-    parser.add_argument('-d', '--data_dir', type=str, 
-                        default='/home/aolab/data/ego4d/')
+    parser.add_argument('-d', '--dataset_dir', type=str,
+                        default='/Users/shionyamadate/Documents/datasets')
     parser.add_argument('-m', '--model', type=str, default="unet",
-                        choices=["unet", "faster_rcnn"])
-    parser.add_argument('-l', '--log_save_dir', type=str, default='./logs/')
+                        choices=["unet"])
+    parser.add_argument('-c', '--ckpt_path', type=str, default='./logs/lightning_logs/2023-04-17T01:03:27.275696/checkpoints/epoch=9-step=2820.ckpt')
 
     return parser.parse_args()
 
@@ -27,51 +27,36 @@ def option_parser():
 def main():
     args = option_parser()
 
-    json_handler = JsonHandler(args.task)
-    json_partial_name = f"{args.data_dir}annotations/{args.task}"
-    json_dict = {
-        "train": json_handler(f"{json_partial_name}_train.json"),
-        "val": json_handler(f"{json_partial_name}_val.json"),
-    }
+    set_seed()
 
     dataset = ObjnessClsDataModule(
-        data_dir=f"{args.data_dir}frames/",
-        json_dict=json_dict,
-        model_name=args.model,
-        batch_size=1,
-        label_mode='corners'    # 'corners' or 'COCO'
+        dataset_dir=args.dataset_dir,
+        dataset_mode='egohos',
+        batch_size=32,
+        with_transform=True,
     )
-
-    # checker = DatasetChecker(dataset)
-    # checker()
 
     module = importlib.import_module(f'models.{args.model}')
     system = module.System()
     classifier = ObjnessClassifier(
-        sys=system
+        model=system.model,
+        loss=system.loss,
+        optimizer=system.optimizer,
+        metric=system.metric,
     )
+    classifier.load_from_checkpoint(args.ckpt_path)
 
     logger = pl.loggers.TensorBoardLogger(
-        save_dir=args.log_save_dir
-    )
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        save_top_k=1,
-        # save_weights_only=True,
-        monitor="train_loss",
-        mode='min',
-        dirpath=args.log_save_dir,
-        filename=args.model
+        save_dir=args.log_dir,
+        version="2023-04-17T01:03:27.275696",
     )
     trainer = pl.Trainer(
         accelerator='auto',
         devices='auto',
-        auto_select_gpus=True,
-        max_epochs=10,
         logger=logger,
-        callbacks=[checkpoint_callback]
     )
 
-    trainer.predict(
+    trainer.test(
         classifier,
         datamodule=dataset,
         # ckpt_path=None
