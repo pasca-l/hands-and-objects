6a98a37fbff4716563e5ddadb8a72471e204287a
pasca-l <shion.yamadate@keio.jp>
2023-04-18 00:59:41 +0900

    put together shared step

--- objectness_classification/system.py
+++ objectness_classification/system.py
@@ -4,9 +4,9 @@ import pytorch_lightning as pl
 class ObjnessClassifier(pl.LightningModule):
     def __init__(
         self,
-        model,
-        loss,
-        optimizer,
+        model=None,
+        loss=None,
+        optimizer=None,
         metric=None,
     ):
         super().__init__()
@@ -14,35 +14,28 @@ class ObjnessClassifier(pl.LightningModule):
         self.loss = loss
         self.optimizer = optimizer
         self.metric = metric
-        self.save_hyperparameters()
 
-    def _shared_step(self, batch, batch_idx):
+    def _shared_step(self, batch, phase="train"):
         frames, labels = batch[0], batch[1]
         logits = self.model(frames)
         loss = self.loss(logits, labels)
         metric_dict = self.metric(logits, labels)
 
+        self.log(f"loss/{phase}", loss, on_step=True, on_epoch=True)
+        metric_dict = {f"{k}/{phase}":v for k,v in metric_dict.items()}
+        self.log_dict(metric_dict, on_step=True, on_epoch=True)
+
         return loss, metric_dict
 
     def training_step(self, batch, batch_idx):
-        loss, _ = self._shared_step(batch, batch_idx)
-
-        self.log("train_loss", loss, on_step=True)
+        loss, _ = self._shared_step(batch, phase="train")
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss, metric_dict = self._shared_step(batch, batch_idx)
-
-        self.log("val_loss", loss, on_step=False, on_epoch=True)
-        metric_dict = {f"val_{k}":v for k,v in metric_dict.items()}
-        self.log_dict(metric_dict, on_step=True)
+        _, _ = self._shared_step(batch, phase="val")
 
     def test_step(self, batch, batch_idx):
-        loss, metric_dict = self._shared_step(batch, batch_idx)
-
-        self.log("test_loss", loss, on_step=False, on_epoch=True)
-        metric_dict = {f"test_{k}":v for k,v in metric_dict.items()}
-        self.log_dict(metric_dict, on_step=True)
+        _, _ = self._shared_step(batch, phase="test")
 
     def configure_optimizers(self):
         return self.optimizer
