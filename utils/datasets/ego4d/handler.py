42b5bda15f6247861cddfb1a25b4d3dfdcc5557f
pasca-l <shion.yamadate@keio.jp>
2023-08-18 13:38:26 +0900

    added function for making test split

--- utils/datasets/ego4d/handler.py
+++ utils/datasets/ego4d/handler.py
@@ -10,27 +10,51 @@ class AnnotationHandler:
         self.ann_file = {
             "train": os.path.join(data_dir, f"{task_name}_train.json"),
             "val": os.path.join(data_dir, f"{task_name}_val.json")
-        }[phase]
+        }
+        self.phase = phase
         self.data_as = data_as
 
     def __call__(self):
         if self.data_as == "dataframe":
-            return self._unpack_manifest_to_df(), self._unpack_json_to_df()
+            df_train = self._unpack_json_to_df("train")
+            df_val = self._unpack_json_to_df("val")
+
+            df = self._create_split_with_test(df_train, df_val)[self.phase]
+
+            return self._unpack_manifest_to_df(), df
 
     def __len__(self):
         if self.data_as == "dataframe":
-            return self._unpack_json_to_df().select(pl.count()).item()
+            _, df = self.__call__()
+            return df.select(pl.count()).item()
 
     def _unpack_manifest_to_df(self):
         return pl.read_csv(self.manifest_file)
 
-    def _unpack_json_to_df(self):
+    def _unpack_json_to_df(self, phase):
         df = pl.read_ndjson(
             bytes("\n".join(
                 [json.dumps(r) for r in json.load(
-                    open(self.ann_file, 'r')
+                    open(self.ann_file[phase], 'r')
                 )["clips"]]
             ), 'utf-8')
         )
 
         return df
+
+    def _create_split_with_test(self, df_train, df_val):
+        df_full = pl.concat([df_train, df_val], how="align")
+
+        test_vids = df_full.select("video_uid").unique(
+            maintain_order=True,
+        ).sample(
+            fraction=0.1, seed=42
+        )
+
+        dfs = {
+            "train": df_train.join(test_vids, on="video_uid", how="anti"),
+            "val": df_val.join(test_vids, on="video_uid", how="anti"),
+            "test": df_full.join(test_vids, on="video_uid", how="semi"),
+        }
+
+        return dfs
