8daf3f56260e85bdb246b72a7fa869c7f1fd4a56
pasca-l <shion.yamadate@keio.jp>
2023-12-11 12:10:25 +0900

    updated parameter update

--- keypoint_estimation/models/__init__.py
+++ keypoint_estimation/models/__init__.py
@@ -26,17 +26,40 @@ def adjust_param(param, pretrain_mode):
 
     if pretrain_mode == "videomae_k400":
         for k, v in param["model"].items():
-            k = k.replace("encoder.patch_embed.proj.", "embeddings.patch_embeddings.projection.")
-            k = k.replace(".proj.", ".value.")
-            k = k.replace(".blocks.", ".layer.")
-            k = k.replace(".attn.", ".attention.")
-            k = k.replace(".norm.", ".layernorm.")
-            k = k.replace(".norm1.", ".layernorm_before.")
-            k = k.replace(".norm2.", ".layernorm_after.")
-            k = k.replace(".q_bias", ".query.bias")
-            k = k.replace(".v_bias", ".value.bias")
-            k = k.replace(".mlp.fc1.", ".intermediate.dense.")
-            k = k.replace(".mlp.fc2.", ".output.dense.")
+            if "decoder" in k:
+                continue
+
+            elif "patch_embed" in k:
+                k = k.replace(
+                    "encoder.patch_embed.proj.", "embedding.projection."
+                )
+
+            elif "blocks" in k:
+                k = k.replace("encoder.blocks.", "encoder.")
+
+                if ".norm1." in k:
+                    k = k.replace(".norm1.", ".ln_before.")
+                elif ".norm2." in k:
+                    k = k.replace(".norm2.", ".ln_after.")
+                elif ".mlp.fc1." in k:
+                    k = k.replace(".mlp.fc1.", ".mlp.0.")
+                elif ".mlp.fc2." in k:
+                    k = k.replace(".mlp.fc2.", ".mlp.3.")
+
+                elif ".attn." in k:
+                    k = k.replace(".attn.", ".mhsa.")
+
+                    if ".proj." in k:
+                        k = k.replace(".proj.", ".w_output.0.")
+                    elif ".qkv." in k:
+                        kq = k.replace(".qkv.", ".w_query.")
+                        kk = k.replace(".qkv.", ".w_key.")
+                        kv = k.replace(".qkv.", ".w_value.")
+                        wq, wk, wv = v.split(split_size=768)
+                        renamed_param[f"vivit.{kq}"] = wq
+                        renamed_param[f"vivit.{kk}"] = wk
+                        renamed_param[f"vivit.{kv}"] = wv
+                        continue
 
             renamed_param[f"vivit.{k}"] = v
 
