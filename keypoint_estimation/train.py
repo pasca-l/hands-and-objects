ad22dd0b148dfd95e57e1a952a6c70c629be4b24
pasca-l <shion.yamadate@keio.jp>
2023-12-12 21:45:20 +0900

    added new command line arguments

--- keypoint_estimation/train.py
+++ keypoint_estimation/train.py
@@ -10,8 +10,9 @@ from system import KeypointEstModule
 
 def option_parser():
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
-        "-d", "--dataset_dir",type=str,
+        "-d", "--dataset_dir", type=str,
         default=os.path.join(
             os.path.expanduser("~"),
             "Documents/datasets",
@@ -20,10 +21,14 @@ def option_parser():
     parser.add_argument(
         "-m", "--model", type=str,
         default="vivit",
-        choices=["vivit", "mctvivit", "tsvivit"],
+        choices=["vivit", "baseline"],
     )
-    parser.add_argument("-l", "--log_dir", type=str, default="./logs/")
-    parser.add_argument("-e", "--exp_dir", type=str, default="")
+    parser.add_argument("--log_dir", type=str, default="./logs/")
+    parser.add_argument("--exp_dir", type=str, default="")
+
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--gpus", nargs="*", type=int)
+    parser.add_argument("--debug", action='store_true')
 
     return parser.parse_args()
 
@@ -47,6 +52,7 @@ def main():
 
     classifier = KeypointEstModule(
         model_name=args.model,
+        epochs=args.epochs,
     )
 
     log_id = datetime.datetime.now().isoformat(timespec="seconds")
@@ -59,12 +65,12 @@ def main():
 
     trainer = L.Trainer(
         deterministic=True,
-        # fast_dev_run=True,
+        fast_dev_run=args.debug,
+        accelerator="auto",
+        devices="auto" if args.gpus is None else args.gpus,
+        max_epochs=args.epochs,
         # limit_train_batches=0.01,
         # limit_val_batches=0.01,
-        accelerator="auto",
-        devices="auto",
-        max_epochs=10,
         logger=logger,
     )
 
