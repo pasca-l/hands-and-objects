1912633c4a14c0511aaa6b693062f098d34791e5
pasca-l <shion.yamadate@keio.jp>
2023-12-13 19:40:17 +0900

    minor change

--- objectness_classification/train.py
+++ objectness_classification/train.py
@@ -20,8 +20,8 @@ def option_parser():
     )
     parser.add_argument(
         "-m", "--model", type=str,
-        default="vivit",
-        choices=["vivit", "baseline", "i3dresnet"],
+        default="unet",
+        choices=["unet", "baseline", "transunet", "segmenter"],
     )
     parser.add_argument("--log_dir", type=str, default="./logs/")
     parser.add_argument("--exp_dir", type=str, default="")
